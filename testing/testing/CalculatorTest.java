e71f6adb5cc5712d785105723cd986824b826155
Alex Wells <wellsedu99@gmail.com>
2023-08-02 15:30:02 -0400

    Updated layout of project. Added test package

---
+++ testing/testing/CalculatorTest.java
@@ -0,0 +1,27 @@
+package testing;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+    private Speaker[] speakers = {new Speaker("A", 4), new Speaker("B", 4)}; 
+    private SeriesCalculator calc = new SeriesCalculator(speakers);
+
+    @Test
+    public void testConstructor()
+    {
+
+    }
+
+    @Test
+    public void testGetNumSpeakers()
+    {
+
+    }
+
+    @Test
+    public void testGetSpeaker()
+    {
+
+    }
+}
---
+++ testing/testing/ParallelTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class ParallelTest {
+    
+
+    @Test
+    public void testParallel() 
+    {
+
+    }
+}
