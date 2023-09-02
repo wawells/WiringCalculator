83ce44dfe91c73d27b86250cdd3ef7453e21f0ce
Alex Wells <wellsedu99@gmail.com>
2023-09-02 16:18:35 -0400

    Updated GUI design and components.

--- struct/struct/WiringInterface.java
+++ struct/struct/WiringInterface.java
@@ -3,29 +3,19 @@ package struct;
 import javax.swing.*;
 import java.awt.*;
 
-public class WiringInterface extends JFrame{
-
-
-
-private JComboBox<String> prefResist;
-private JTextField speakerList;
-private JTextField resultField; 
-private JButton addSpeaker;
-private JButton editSpeaker;
-private JButton deleteSpeaker;
-private JButton clearSpeakers;
+public class WiringInterface 
+{
+private JFrame frame;
 private JPanel contentPanel;
-private BorderLayout border;
-//private WireListener listener;
-
+private JTextArea speakerList;
+private JButton addSpeaker, editSpeaker, deleteSpeaker, clearSpeakers, generateButton;
+private JTextField resultField;
 
 public WiringInterface() 
 {
     setup();
-    addComponents();
-
-    pack();
-    setVisible(true);
+    frame.add(contentPanel);
+    frame.setVisible(true);
 }
 
 
@@ -37,46 +27,85 @@ public static void main(String[] args)
 
 private void setup() 
 {
-    setMinimumSize(new Dimension(600, 300));
-    setTitle("Wiring Calculator");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame = new JFrame("Wiring Calculator");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(400, 600);
 
-    contentPanel = new JPanel();
-
-    border = new BorderLayout();
-    contentPanel.setLayout(border);
-
-    prefResist = new JComboBox<String>();
-    speakerList = new JTextField();
-    resultField = new JTextField("results here"); //TODO only display if calculate selected. Can only be selected if 1 speaker in system
+    contentPanel = new JPanel(new BorderLayout());
     
+    JPanel spacerPanel = new JPanel(); //move this declaration to above. Initialize here
+    spacerPanel.setPreferredSize(new Dimension(10,10));
+
+    speakerList = new JTextArea(10, 30);
+    speakerList.setEditable(true);
+    JScrollPane scrollPane = new JScrollPane(speakerList);
     
+    JPanel buttonPanel = new JPanel(new GridBagLayout());  
+    GridBagConstraints gbc = new GridBagConstraints();
+    gbc.fill = GridBagConstraints.HORIZONTAL;
+    gbc.weightx = 1.0; //make buttons equally spaced
+
     addSpeaker = new JButton("Add");
     editSpeaker = new JButton("Edit");
     deleteSpeaker = new JButton("Delete");
     clearSpeakers = new JButton("Clear");
 
+    Dimension buttonSize = new Dimension(100, 50);
+
+    addSpeaker.setPreferredSize(buttonSize);
+    editSpeaker.setPreferredSize(buttonSize);
+    deleteSpeaker.setPreferredSize(buttonSize);
+    clearSpeakers.setPreferredSize(buttonSize);
+
+
+    gbc.gridx = 0;
+    buttonPanel.add(addSpeaker, gbc);
+
+    gbc.gridx = 1;
+    buttonPanel.add(editSpeaker, gbc);
+
+    gbc.gridx = 2;
+    buttonPanel.add(deleteSpeaker, gbc);
+
+    gbc.gridx = 3;
+    buttonPanel.add(clearSpeakers, gbc);
+
+
+    JPanel resultPanel = new JPanel(new BorderLayout());
+    resultField = new JTextField(20);
+    resultField.setPreferredSize(new Dimension(300, 30));
+    generateButton = new JButton("Show Wiring");
+
+    //top half of app
+    JPanel topPanel = new JPanel(new BorderLayout());
+    topPanel.add(scrollPane, BorderLayout.NORTH);
+    topPanel.add(buttonPanel, BorderLayout.CENTER);
+
+
+    //bottom half of app
+    resultPanel.add(resultField, BorderLayout.NORTH);
+    resultPanel.add(generateButton, BorderLayout.SOUTH);
+    
+    contentPanel.add(spacerPanel, BorderLayout.NORTH);
+    contentPanel.add(topPanel, BorderLayout.NORTH);
+    contentPanel.add(resultPanel, BorderLayout.SOUTH);
+
 
     
+
+
+
+
+
+
+
+
+    
+    //TODO add action listeners.
     //listener = new WireListener(this.prefLength, this.prefField, this.resultField, this.copyB, this.passB);
-
-    //copyB.addActionListener(listener);
-   // passB.addActionListener(listener);  
-  
 }
 
-private void addComponents()
-{
-    contentPanel.add(speakerList, BorderLayout.NORTH);
-    //TODO add speaker buttons, listeners to buttons, and determine if I need an array of the speakers here?
-    //contentPanel.add(prefField, BorderLayout.CENTER);
-    //contentPanel.add(passB, BorderLayout.WEST);
 
-    contentPanel.add(resultField, BorderLayout.SOUTH);
-    //contentPanel.add(copyB, BorderLayout.EAST);
-
-    add(contentPanel);
-}
 
 
 
